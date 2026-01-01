b4dd81fafad6ba42dd1f84cb760eb8be4e8b325f
payam <payam.shr@yahoo.com>
2026-01-01 21:40:48 +0100

    pile line added

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '@/lib/stores/authStore';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
 
 // Create axios instance
 export const apiClient = axios.create({
